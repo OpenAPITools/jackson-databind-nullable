8bab92921ff77ce824d16e12ea02c29f64c55ed1
Tobias Fischer <tobias.carl.fischer@gmail.com>
2022-10-24 04:49:49 +0200

    #34 adds support for Collection<JsonNullable<T>> in the JsonNullableValueExtractor (#35)

--- src/main/java/org/openapitools/jackson/nullable/JsonNullableValueExtractor.java
+++ src/main/java/org/openapitools/jackson/nullable/JsonNullableValueExtractor.java
@@ -3,6 +3,7 @@ package org.openapitools.jackson.nullable;
 import javax.validation.valueextraction.ExtractedValue;
 import javax.validation.valueextraction.UnwrapByDefault;
 import javax.validation.valueextraction.ValueExtractor;
+import java.util.Collection;
 
 /**
  * Extractor for JsonNullable
@@ -12,7 +13,16 @@ public class JsonNullableValueExtractor implements ValueExtractor<JsonNullable<@
     @Override
     public void extractValues(JsonNullable<?> originalValue, ValueReceiver receiver) {
         if (originalValue.isPresent()) {
-            receiver.value(null, originalValue.get());
+            Object unwrapped = originalValue.get();
+            if (unwrapped instanceof Collection<?>) {
+                Collection<?> unwrappedList = (Collection<?>) unwrapped;
+                Object[] objects = unwrappedList.toArray();
+                for (int i = 0; i < objects.length; i++) {
+                    receiver.indexedValue("<list element>", i, objects[i]);
+                }
+            } else {
+                receiver.value(null, originalValue.get());
+            }
         }
     }
 }
--- src/test/java/org/openapitools/jackson/nullable/JsonNullableValueExtractorTest.java
+++ src/test/java/org/openapitools/jackson/nullable/JsonNullableValueExtractorTest.java
@@ -3,16 +3,18 @@ package org.openapitools.jackson.nullable;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import javax.validation.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class JsonNullableValueExtractorTest {
 
@@ -52,6 +54,27 @@ public class JsonNullableValueExtractorTest {
         assertEquals(1, violations.size());
     }
 
+    // ensure that JsonNullable<Collection<T>> gets unwrapped and the collection items are validated as well
+    @Test
+    public void testUnwrapList() {
+        Car aCar = new Car();
+
+        // test for java.util.List
+        aCar.addWheel(new Wheel("all"));
+        aCar.addWheel(new Wheel());
+        aCar.addWheel(new Wheel("some"));
+        aCar.addWheel(new Wheel());
+
+        // test for java.util.Set
+        aCar.addPerson(new Person());
+
+        Set<ConstraintViolation<Car>> validationResult = validator.validate(aCar);
+        assertEquals(3, validationResult.size());
+        assertTrue(validationResult.stream().anyMatch(c -> c.getPropertyPath().toString().equals("wheels[1].screws") && c.getConstraintDescriptor().getAnnotation().annotationType().getSimpleName().equals("NotNull")));
+        assertTrue(validationResult.stream().anyMatch(c -> c.getPropertyPath().toString().equals("wheels[3].screws") && c.getConstraintDescriptor().getAnnotation().annotationType().getSimpleName().equals("NotNull")));
+        assertTrue(validationResult.stream().anyMatch(c -> c.getPropertyPath().toString().equals("persons[0].role") && c.getConstraintDescriptor().getAnnotation().annotationType().getSimpleName().equals("NotNull")));
+    }
+
 
     private static class UnitIssue2 {
         @Size(max = 10)
@@ -80,6 +103,7 @@ public class JsonNullableValueExtractorTest {
         }
     }
 
+
     private static class UnitIssue3 {
         @NotNull
         private JsonNullable<String> notNullString = JsonNullable.undefined();
@@ -88,4 +112,52 @@ public class JsonNullableValueExtractorTest {
             notNullString = JsonNullable.of(value);
         }
     }
+
+
+    private static class Car {
+        @Valid
+        private JsonNullable<List<Wheel>> wheels = JsonNullable.undefined();
+
+        @Valid
+        private JsonNullable<Set<Person>> persons = JsonNullable.undefined();
+
+        public void addWheel(Wheel wheel) {
+            if (wheels == null || !wheels.isPresent()) {
+                wheels = JsonNullable.of(new ArrayList<>());
+            }
+            wheels.get().add(wheel);
+        }
+
+        public void addPerson(Person person) {
+            if (persons == null || !persons.isPresent()) {
+                persons = JsonNullable.of(new HashSet<>());
+            }
+            persons.get().add(person);
+        }
+    }
+
+    private static class Wheel {
+        @NotNull
+        private String screws;
+
+        public Wheel() {
+        }
+
+        public Wheel(String screws) {
+            this.screws = screws;
+        }
+    }
+
+    private static class Person {
+        @NotNull
+        private String role;
+
+        public Person() {
+        }
+
+        public Person(String role) {
+            this.role = role;
+        }
+    }
+
 }
