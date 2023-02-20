3f9974f679f74babc1ffc3b1f22911e473658952
Dennis Melzer <dennis.melzer@de.bosch.com>
2023-02-20 18:32:49 +0100

    Fix broken unwrap bean validation and adapt tests (#50)

--- src/main/java/org/openapitools/jackson/nullable/JsonNullableJakartaValueExtractor.java
+++ src/main/java/org/openapitools/jackson/nullable/JsonNullableJakartaValueExtractor.java
@@ -11,6 +11,6 @@ import jakarta.validation.valueextraction.ValueExtractor;
 public class JsonNullableJakartaValueExtractor implements ValueExtractor<JsonNullable<@ExtractedValue ?>> {
     @Override
     public void extractValues(JsonNullable<?> originalValue, ValueReceiver receiver) {
-        JsonNullableValueExtractorHelper.extractValues(originalValue, receiver::indexedValue, receiver::value);
+        JsonNullableValueExtractorHelper.extractValues(originalValue, receiver::value);
     }
 }
