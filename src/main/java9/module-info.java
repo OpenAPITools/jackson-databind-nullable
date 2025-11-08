392dd35112ede644c6dd837e19029d2d84765ddd
Tilman Holube <tilman@holube.de>
2025-11-09 00:52:00 +0100

    Leave in SPI entries and comment out optional ones

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -4,4 +4,10 @@ module org.openapitools.jackson.nullable {
     requires static java.validation;
 
     exports org.openapitools.jackson.nullable;
+
+    provides com.fasterxml.jackson.databind.Module with org.openapitools.jackson.nullable.JsonNullableModule;
+    // These service providers for optional dependencies can be added, if baseline is 25+ or javax support is dropped
+    // see https://github.com/OpenAPITools/jackson-databind-nullable/issues/100
+    //provides javax.validation.valueextraction.ValueExtractor with org.openapitools.jackson.nullable.JsonNullableValueExtractor;
+    //provides jakarta.validation.valueextraction.ValueExtractor with org.openapitools.jackson.nullable.JsonNullableJakartaValueExtractor;
 }
